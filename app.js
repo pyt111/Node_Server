b423fc5414ce081d87af38d61afbbaf7d7002407
wangshuang <584702675@qq.com>
2020-01-13 15:45:04 +0800

    nginx

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ app.use(express.static(utils.getStaticPath()));
 
 // app.use('/', indexRouter);
 // app.use('/', Particular);
-// app.use('/', indexRouter);
-app.use('/vue-web', indexRouter);
+app.use('/', indexRouter);
+// app.use('/vue-web', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Configuration', Configuration);
 app.use('/queryMeta', queryMeta);
