3a8ea4fd6ab2dfcbc36f4b952e0c9896c4591e78
wangshuang <584702675@qq.com>
2020-01-13 15:14:53 +0800

    nginx

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/', function(req, res, next) {
   // res.sendFile(nginxUrl);
   // res.json(data)
   // res.render('index', { title: 'Express' });
-  res.redirect('/vue-web/index')
+  res.redirect('/vue-web/index.html')
 });
 
 module.exports = router;
